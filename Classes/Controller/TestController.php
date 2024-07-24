65caa3c8c13ef128ccb79fbc6419d12cec98bd2e
Codecrafter404 <codecrafter404@github.com>
2024-07-24 16:02:21 +0200

    WIP: splitted fusion integration/presentation, added test path

---
+++ Classes/Controller/TestController.php
@@ -0,0 +1,24 @@
+<?php
+namespace Sandstorm\UserManagement\Controller;
+use Neos\Flow\Mvc\Controller\ActionController;
+use Neos\Neos\View\FusionView;
+
+class TestController extends ActionController
+{
+
+    public function testAction(): void {
+        $this->view = new FusionView();
+        $this->view->setControllerContext($this->controllerContext);
+        $this->view->setFusionPathPatterns([
+            'resource://Neos.Fusion/Private/Fusion/Root.fusion',
+            'resource://Sandstorm.UserManagement/Private/Fusion/Root.fusion'
+        ]);
+        $this->view->setFusionPath('<Sandstorm.UserManagement:Test>');
+
+        // assign the props here
+        $this->view->assign('test', 'hello world!');
+
+        $this->view->render();
+    }
+
+}
